016db203c0e0bc0239837c67eb9904e183005d8b
Nhut Phan <thanquan@gmail.com>
2020-10-15 21:59:44 +0700

    Add using assets

--- src/components/hello-stencil/hello-stencil.tsx
+++ src/components/hello-stencil/hello-stencil.tsx
@@ -1,14 +1,16 @@
-import { Component, h } from '@stencil/core';
+import { Component, h, getAssetPath } from '@stencil/core';
 
 @Component({
   tag: 'hello-stencil',
   styleUrl: 'hello-stencil.styl',
+  assetsDirs: ['assets'],
 })
 export class HelloStencil {
   render() {
     return (
       <div>
         <p>Hello Stencil!</p>
+        <svg-render src={getAssetPath(`./assets/speech-bubble.svg`)}></svg-render>
       </div>
     );
   }
